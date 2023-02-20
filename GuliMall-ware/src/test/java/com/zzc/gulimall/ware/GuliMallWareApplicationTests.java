ff8c23bedb44739306d2daeb8ffbb3a68de9b297
ValcanoZz <zecanzhengwyyx@163.com>
2023-02-20 16:02:56 +0800

    项目初始结构搭建

---
+++ GuliMall-ware/src/test/java/com/zzc/gulimall/ware/GuliMallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzc.gulimall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliMallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
